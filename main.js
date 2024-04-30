953d4bb7e2672663f7ca9f42201bdcffbdaec907
damirusso95 <damirusso95@gmail.com>
2024-04-30 14:52:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata 10x10 (con 100 celle).
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// :roccia: MILESTONE #1
+// Create la vostra griglia in HTML e CSS, in maniera grossolana. Vi servirà solo per testare le classi, come visto in mattinata con la griglia 3x3.
+
+// :roccia: MILESTONE #2
+// Commentate l'HTML e iniettatelo tramite un ciclo in JS, senza pensare al click o alle funzioni.
+
+// :roccia: MILESTONE #3
+// Aggiungete la gestione dell'evento di click, che stampi il numero e cambi il colore della cella.
+
+// :roccia: MILESTONE #4
+// Spostate alcuni elementi in funzioni, ad esempio la creazione del quadrato e/o l'event listener.
+
+
