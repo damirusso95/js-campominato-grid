5183fccb1f5955a105b622056bc93d74ef87a457
damirusso95 <damirusso95@gmail.com>
2024-05-02 11:24:41 +0200

    bonus- bottone strat- select difficoltà

--- main.js
+++ main.js
@@ -15,38 +15,46 @@
 
 // :roccia: MILESTONE #4
 // Spostate alcuni elementi in funzioni, ad esempio la creazione del quadrato e/o l'event listener.
-alert("1 facile - 2 normale - 3 difficile");
-let select = parseInt(prompt("seleziona la difficoltà"));
-if (select == 1) {
-    let griglia = document.getElementById("grid");
-    // creo un ciclo for
-    for (let i = 1; i <= 100; i++) {
-        // // salvo e creo il quadrato nel virtual dom
-        // let square = document.createElement("div");
+// alert("1 facile - 2 normale - 3 difficile");
+// let select = parseInt(prompt("seleziona la difficoltà"));
+let btnStart = document.getElementById("btnStart");
+let select = document.getElementById("difficoltà");
 
-        // // aggiungo del testo che prendere il valore di "i" all'interno del quadrato
-        // square.innerText = i
+btnStart.addEventListener("click", function () {
+    grid.innerHTML=""
+    gridNorm.innerHTML=""
+    gridHard.innerHTML=""
 
-        // ad ogni ciclo richiamo la funzione per creare il quadrato
-        let elemento = CreaQuadrato(i);
+    if (select.value == "easy") {
+        let griglia = document.getElementById("grid");
+        // creo un ciclo for
+        for (let i = 1; i <= 100; i++) {
+            // // salvo e creo il quadrato nel virtual dom
+            // let square = document.createElement("div");
 
-        // // aggiungo la classe square
-        // square.classList.add("square");
+            // // aggiungo del testo che prendere il valore di "i" all'interno del quadrato
+            // square.innerText = i
 
-        // alla griglia aggiungo con append in quadrato nel dom
-        griglia.append(elemento);
+            // ad ogni ciclo richiamo la funzione per creare il quadrato
+            let elemento = CreaQuadrato(i);
 
-        // elemento.addEventListener("click", function(){
-        // console.log("click");
+            // // aggiungo la classe square
+            // square.classList.add("square");
 
-        // });
+            // alla griglia aggiungo con append in quadrato nel dom
+            griglia.append(elemento);
 
+            // elemento.addEventListener("click", function(){
+            // console.log("click");
+
+            // });
+
+
+        }
 
     }
-
-}
-else if (select == 2) {
-    // difficoltà normale
+    else if (select.value == "medium") {
+        // difficoltà normale
         let griglia = document.getElementById("gridNorm");
 
         for (let i = 1; i <= 81; i++) {
@@ -57,8 +65,8 @@ else if (select == 2) {
     }
 
 
- else if (select == 3) {
-    // difficoltà Difficile
+    else if (select.value == "hard") {
+        // difficoltà Difficile
         let griglia = document.getElementById("gridHard");
 
         for (let i = 1; i <= 49; i++) {
@@ -69,73 +77,76 @@ else if (select == 2) {
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-btnStart.addEventListener("click", function () {
-    // salvo la variabile sul dom virtuale della pagina
-    let griglia = document.getElementById("grid");
-    // creo un ciclo for
-    for (let i = 1; i <= 100; i++) {
-        // // salvo e creo il quadrato nel virtual dom
-        // let square = document.createElement("div");
-
-        // // aggiungo del testo che prendere il valore di "i" all'interno del quadrato
-        // square.innerText = i
-
-        // ad ogni ciclo richiamo la funzione per creare il quadrato
-        let elemento = CreaQuadrato(i);
-
-        // // aggiungo la classe square
-        // square.classList.add("square");
-
-        // alla griglia aggiungo con append in quadrato nel dom
-        griglia.append(elemento);
-
-        // elemento.addEventListener("click", function(){
-        // console.log("click");
-
-        // });
-
-
-    }
-
 })
 
 
-// difficoltà normale
-btnNormale.addEventListener("click", function () {
-    let griglia = document.getElementById("gridNorm");
-
-    for (let i = 1; i <= 81; i++) {
-
-        let elemento = CreaQuadrato(i);
-        griglia.append(elemento);
-    }
-})
 
 
-// difficoltà Difficile
-btnHard.addEventListener("click", function () {
-    let griglia = document.getElementById("gridHard");
 
-    for (let i = 1; i <= 49; i++) {
 
-        let elemento = CreaQuadrato(i);
-        griglia.append(elemento);
-    }
 
-})
 
+
+//     btnStart.addEventListener("click", function() {
+
+
+
+
+
+
+// btnStart.addEventListener("click", function () {
+//     // salvo la variabile sul dom virtuale della pagina
+//     let griglia = document.getElementById("grid");
+//     // creo un ciclo for
+//     for (let i = 1; i <= 100; i++) {
+//         // // salvo e creo il quadrato nel virtual dom
+//         // let square = document.createElement("div");
+
+//         // // aggiungo del testo che prendere il valore di "i" all'interno del quadrato
+//         // square.innerText = i
+
+//         // ad ogni ciclo richiamo la funzione per creare il quadrato
+//         let elemento = CreaQuadrato(i);
+
+//         // // aggiungo la classe square
+//         // square.classList.add("square");
+
+//         // alla griglia aggiungo con append in quadrato nel dom
+//         griglia.append(elemento);
+
+//         // elemento.addEventListener("click", function(){
+//         // console.log("click");
+
+//         // });
+
+
+//     }
+
+// })
+
+
+// // difficoltà normale
+// btnNormale.addEventListener("click", function () {
+//     let griglia = document.getElementById("gridNorm");
+
+//     for (let i = 1; i <= 81; i++) {
+
+//         let elemento = CreaQuadrato(i);
+//         griglia.append(elemento);
+//     }
+// })
+
+
+// // difficoltà Difficile
+// btnHard.addEventListener("click", function () {
+//     let griglia = document.getElementById("gridHard");
+
+//     for (let i = 1; i <= 49; i++) {
+
+//         let elemento = CreaQuadrato(i);
+//         griglia.append(elemento);
+//     }
+
+// })
+
+// })
